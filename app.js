64fe1d3d51909aac6249b8c37cd62303925d8ceb
Velta Inc <velta.inc.la@gmail.com>
2024-09-26 00:27:03 +0000

    Update file app.js

--- app.js
+++ app.js
@@ -5,8 +5,6 @@ import cors from "cors";
 const app = express();
 
 app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 app.use("/public", express.static("public"));
 
 
